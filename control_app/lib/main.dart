253376bad1b1cee31254638ccebf0801fb4d3e95
hackerFaruk <onuralparslan97@gmail.com>
2023-03-11 22:55:11 +0300

    first commit

---
+++ control_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Motor Kontrol'),
+      ),
+    ),
+  )
+  );
+}
+
