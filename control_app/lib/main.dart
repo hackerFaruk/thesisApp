9452bf5fcf6b163b350f0d35537e7c76137fcc60
hackerFaruk <onuralparslan97@gmail.com>
2023-03-11 23:01:55 +0300

    basic coloring

--- control_app/lib/main.dart
+++ control_app/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const
+  runApp(
   MaterialApp(
     home: Scaffold(
+      backgroundColor: const  Color.fromARGB(255, 100, 104, 105) ,
       appBar: AppBar(
-        title: Text('Motor Kontrol'),
+        title: const Text('Motor Kontrol'),
+        backgroundColor: const Color.fromARGB(255, 32, 197, 216),
       ),
     ),
   )
