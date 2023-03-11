e0e4e7c2f14d20cfb75c7924278c13a56e4297a8
hackerFaruk <onuralparslan97@gmail.com>
2023-03-11 23:11:06 +0300

    centered picture

--- control_app/lib/main.dart
+++ control_app/lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-  MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: const  Color.fromARGB(255, 100, 104, 105) ,
+      backgroundColor: const Color.fromARGB(255, 100, 104, 105),
       appBar: AppBar(
         title: const Text('Motor Kontrol'),
         backgroundColor: const Color.fromARGB(255, 32, 197, 216),
       ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+              'https://www.tutorialkart.com/wp-content/uploads/2021/11/flutter-scaffold-iphone-1.png'),
+        ),
+      ),
+    
+    
     ),
-  )
-  );
+  ));
 }
-
